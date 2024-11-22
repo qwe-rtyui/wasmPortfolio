f034af9011c0687cfac52fb680f74bfebbbe3748
qwe-rtyui <kesleyjuan.dev@gmail.com>
2024-11-22 02:29:51 -0300

    finalizado o app indo para prod

---
+++ wwwroot/js/script.js
@@ -0,0 +1,14 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll('a[href^="#"]');
+    links.forEach(link => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+            const targetId = link.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: "smooth"
+            });
+        });
+    });
+});
